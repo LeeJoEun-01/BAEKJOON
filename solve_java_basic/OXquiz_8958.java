4d60c0311401bfbbaef8b48f6e03b265fcce5f97
LeeJoEun-01 <joeun1005@naver.com>
2022-06-30 22:32:12 +0900

    fault solve OTL

---
+++ solve_java_basic/OXquiz_8958.java
@@ -0,0 +1,40 @@
+package baekjoon.solve_java_basic;
+
+import java.util.Scanner;
+
+public class OXquiz_8958 {
+
+    public static void main(String[] args) {
+        int num = 0;
+        String str = " ";
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("테스트 케이스 개수를 입력해주세요! ");
+        num = sc.nextInt();
+        
+        int[] result = new int[num];
+
+        for( int i=0; i<num; i++) {
+            System.out.println("OX배열 테스트 케이스를 입력해주세요! ");
+            str = sc.next();
+            String[] arr;
+            arr = str.split("");
+            System.out.println(arr.length);
+            int sum = 0;
+            for( int j=0; j<arr.length; j++) {
+                char target = str.charAt(j);
+                if( target == 'O') {
+                    sum += (j+1);
+                }
+                System.out.println(sum);
+            }
+            result[i] = sum ;
+        }
+        sc.close();
+
+        for (int answer : result) {
+            System.out.println(answer);
+        }
+    }
+}
