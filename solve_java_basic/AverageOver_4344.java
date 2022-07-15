3dacca0b11a71ef1036e3f26d14ad9987aa73920
LeeJoEun-01 <joeun1005@naver.com>
2022-07-15 23:24:04 +0900

    ~ing prolbem

--- solve_java_basic/AverageOver_4344.java
+++ solve_java_basic/AverageOver_4344.java
@@ -10,37 +10,39 @@ public class AverageOver_4344 {
         float sum = 0.0f;
         float avg = 0.0f;
         float result = 0.0f;
+        float[] ansArray;
 
-        System.out.println("테스트 케이스의 개수를 입력해주세요!");
+        // System.out.println("테스트 케이스의 개수를 입력해주세요!");
         numC = sc.nextInt();
+        ansArray = new float[numC];
         
         for(int i=0; i<numC; i++) {
-            System.out.println("학생 수를 입력해주세요!");
+            // System.out.println("학생 수를 입력해주세요!");
             numN = sc.nextInt();
             int[] scoreArr = new int[numN];
             sum = 0;
-            System.out.println("학생들의 점수를 입력해주세요!");
+            // System.out.println("학생들의 점수를 입력해주세요!");
             for(int j=0; j<numN; j++) {
                 scoreArr[j] = sc.nextInt();
                 sum = sum + scoreArr[j];
             }
-            System.out.println(sum);
+
             int overStd = 0;
             avg = sum/numN;
-            System.out.println(avg);
+            // System.out.println(avg);
 
             for(int h=0; h<numN; h++) {
-                System.out.println(scoreArr[h]);
+                // System.out.println(scoreArr[h]);
                 if(scoreArr[h] > avg) {
                     overStd++;
                 }
             }
-            result = overStd/numN;
-            System.out.println(result);
-
-            //System.out.println((Math.round(result*1000)/1000.0)+"%");
+            result = (float) overStd/numN*100;
+            ansArray[i] = result;
+        }
+        for( float target:ansArray) {
+            System.out.println(Math.round(target*1000)/1000.0+ "%");
         }
 
     }
-
 }
