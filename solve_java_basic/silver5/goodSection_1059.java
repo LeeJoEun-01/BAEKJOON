3578383f3dc2b7d7eca6df90800b1910c4134499
LeeJoEun-01 <joeun1005@naver.com>
2022-07-29 16:59:28 +0900

    Slove Silver5 -1059 ~ing

--- solve_java_basic/silver5/goodSection_1059.java
+++ solve_java_basic/silver5/goodSection_1059.java
@@ -7,11 +7,12 @@ public class goodSection_1059 {
     Scanner sc = new Scanner(System.in);
 
     int size = 0; // 집합의 크기
-    String arrS[] = new String[size];  // 집합 S
     int target = 0; // 타겟: n
-    int result = 0;
+    int result = 0; //정답
 
     size = sc.nextInt();
+    String arrS[] = new String[size];  // 집합 S
+
     for(int i=0; i<size; i++){
       arrS[i] = sc.next();
     }
@@ -20,13 +21,19 @@ public class goodSection_1059 {
     // 1. target(=n)을 기준으로 왼쪽 오른쪽 값 찾기 => left < target < right
     // 그런데 left나 right 값이 target과 동일하면 result = 0임으로 프로세스 종료
     int left = 0;
-    int right = 0;
+    int right = 1001;
     for(int i=0; i<size; i++){
       int move = Integer.parseInt(arrS[i]);
-      if(move > target){
-
-      } else if (move < target) {
-
+      if(move < target) { // left = target보다 작은 수들 중에서 제인 큰 수
+        if(move > left) {
+          left = move;
+          // System.out.println(left);
+        }
+      } else if (move > target) { // right = target보다 큰 수들 중에서 제일 작은 수
+        if(move < right) {
+          right = move;
+          // System.out.println(right);
+        }
       } else{ // move == target
         result = 0;
       }
@@ -41,5 +48,7 @@ public class goodSection_1059 {
     } else { // 둘 다 아닌 경우
       result = (target-left)*(right-target)-1;
     }
+
+    System.out.println(result);
   }
 }
