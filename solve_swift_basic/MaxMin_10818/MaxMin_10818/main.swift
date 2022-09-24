42ef820c11a82c1b822c714796ad478ee9d76917
LeeJoEun-01 <joeun1005@naver.com>
2022-09-24 16:46:58 +0900

    Solve divisior_1037

--- solve_swift_basic/MaxMin_10818/MaxMin_10818/main.swift
+++ solve_swift_basic/MaxMin_10818/MaxMin_10818/main.swift
@@ -7,23 +7,56 @@
 
 import Foundation
 
-let numN = Int(readLine()!)!
-let intArr = readLine()!.split(separator: " ").map { Int(String($0))! }
-//print(intArr)
-var maxNum: Int = intArr[0]
-var minNum: Int = intArr[0]
-var target: Int = 0
+// 최대최소 -10818
 
-for i in 0...(numN-1) {
-    target = intArr[i]
-    if target <= maxNum {
-        if target < minNum {
-            minNum = target
+//let numN = Int(readLine()!)!
+//let intArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+////print(intArr)
+//var maxNum: Int = intArr[0]
+//var minNum: Int = intArr[0]
+//var target: Int = 0
+//
+//for i in 0...(numN-1) {
+//    target = intArr[i]
+//    if target <= maxNum {
+//        if target < minNum {
+//            minNum = target
+//        }
+//    } else {
+//        maxNum = target
+//    }
+//}
+//
+//print("\(minNum) \(maxNum)")
+
+//약수 -1037
+
+let numN = Int(readLine()!)! // N의 진짜 약수의 개수
+var divisorList = readLine()!.split(separator: " ").map { Int(String($0))! } //N의 진짜 약수 원소들
+
+var maxNum: Int = divisorList[0]
+var minNum: Int = divisorList[0]
+var target: Int = 0
+var ans: Int = 0
+
+if divisorList.count == 1 {
+    ans = divisorList[0]*divisorList[0]
+} else {
+    for i in 0...(numN-1) {
+        target = divisorList[i]
+        if target <= maxNum {
+            if target < minNum {
+                minNum = target
+            }
+        } else {
+            maxNum = target
         }
-    } else {
-        maxNum = target
     }
+    ans = maxNum*minNum
+
 }
 
-print("\(minNum) \(maxNum)")
+print(ans)
+
+//print("\(divisorList.min()! * divisorList.max()!)")
 
