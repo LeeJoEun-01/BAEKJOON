d7371e33de641b4f2fee6d20d9f33f98c34884ae
LeeJoEun-01 <joeun1005@naver.com>
2023-01-04 16:53:53 +0900

    백준 1308.D-day (Swift) 런타임 에러...

--- solve_swift_basic/WordDivide_1251/WordDivide_1251/main.swift
+++ solve_swift_basic/WordDivide_1251/WordDivide_1251/main.swift
@@ -7,29 +7,51 @@
 
 import Foundation
 
-let input = Array(readLine()!) // (띄어쓰기없이) 입력받아 배열로 return
-let len = input.count
 
-//var ans : [String] = []
-var ans = "-"
-for i in 0..<len-2 {
-    for j in i+1..<len-1 {
-        let first = Array(input[0...i].reversed())
-        let second = Array(input[i+1...j].reversed())
-        let third = Array(input[j+1...len-1].reversed())
-        let comb = (first+second+third).map{String($0)}.joined()
+// WordDivide_1251
+//let input = Array(readLine()!) // (띄어쓰기없이) 입력받아 배열로 return
+//let len = input.count
+//
+////var ans : [String] = []
+//var ans = "-"
+//for i in 0..<len-2 {
+//    for j in i+1..<len-1 {
+//        let first = Array(input[0...i].reversed())
+//        let second = Array(input[i+1...j].reversed())
+//        let third = Array(input[j+1...len-1].reversed())
+//        let comb = (first+second+third).map{String($0)}.joined()
+//
+//        //ans.append(comb)
+//        // 맨 마지막에 sorted로 값을 뽑는 것보다 if 문을 사용하는 것이 메모리가 더 적다.
+//        if ans == "-" || ans > comb {
+//            ans = comb
+//        }
+//    }
+//}
+////let result = ans.sorted(by: <)
+//
+////print(result[0])
+//
+//print(ans)
 
-        //ans.append(comb)
-        // 맨 마지막에 sorted로 값을 뽑는 것보다 if 문을 사용하는 것이 메모리가 더 적다.
-        if ans == "-" || ans > comb {
-            ans = comb
-        }
-    }
+
+// D-Day_1308
+let currentDay = readLine()!
+let dDay = readLine()!
+
+let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+let startDate = dateFormatter.date(from:currentDay)
+let endDate = dateFormatter.date(from:dDay)
+
+//print(startDate ?? 0)
+//print(endDate ?? 0)
+
+let calendar = Calendar.current
+let dateGap: Int = calendar.dateComponents([.day], from: startDate!, to: endDate!).day ?? -1
+
+if (dateGap+1)/365 >= 1000 {
+    print("gg")
+} else {
+    print("D-\(dateGap)")
 }
-//let result = ans.sorted(by: <)
-
-//print(result[0])
-
-print(ans)
-
-
