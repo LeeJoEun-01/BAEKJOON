0e918a3de78783a8efbe0ba33fc0638f1272e96c
LeeJoEun-01 <joeun1005@naver.com>
2023-02-04 22:43:41 +0900

    백준 c언어로 풀기 (week1)
    단계별 > 입출력과 사칙연산

---
+++ solve_c/level1.c
@@ -0,0 +1,130 @@
+// #include <stdio.h>
+
+// 1000_A+B
+
+// int main(void)
+// {
+//   int a;
+//   int b;
+//   scanf("%d %d", &a, &b);
+//   printf("%d \n", a + b);
+//   return 0;
+// }
+
+// 1008_A/B
+
+// int main(void)
+// {
+//   double a;
+//   double b;
+//   scanf("%lf %lf", &a, &b);
+//   printf("%.9lf \n", a / b);
+//   return 0;
+// }
+
+// 10869_사칙연산
+
+// int main(void)
+// {
+//   int a;
+//   int b;
+//   scanf("%d %d", &a, &b);
+//   printf("%d \n", a + b);
+//   printf("%d \n", a - b);
+//   printf("%d \n", a * b);
+//   printf("%d\n", a / b);
+//   printf("%d\n", a % b);
+//   return 0;
+// }
+
+// 10926_??!
+
+// int main(void)
+// {
+//   char name[50];
+//   scanf("%s", &name);
+//   printf("%s\?\?!\n", name);
+//   return 0;
+// }
+
+// 18108_1998년생인 내가 태국에서는 2541년생?!
+
+// int main(void)
+// {
+//   int year;
+//   scanf("%d", &year);
+//   year = year - 543;
+//   printf("%d\n", year);
+//   return 0;
+// }
+
+// 3003_킹, 퀀, 룩, 비숍, 나이트, 폰
+
+// #include <stdio.h>
+// int main(void)
+// {
+//   int ki;
+//   int qu;
+//   int lo;
+//   int bi;
+//   int ni;
+//   int po;
+//   scanf("%d %d %d %d %d %d", &ki, &qu, &lo, &bi, &ni, &po);
+//   printf("%d %d %d %d %d %d\n", 1 - ki, 1 - qu, 2 - lo, 2 - bi, 2 - ni, 8 - po);
+//   return 0;
+// }
+
+// 10430_나머지
+
+// #include <stdio.h>
+// int main(void)
+// {
+//   int num1;
+//   int num2;
+//   scanf("%d %d", &num1, &num2);
+//   int n1 = num2 / 100;
+//   int n3 = num2 % 10;
+//   int n2 = (num2 % 100 - n3) / 10;
+//   // printf("%d %d %d\n", n1, n2, n3);
+//   printf("%d\n%d\n%d\n%d\n", num1 * n3, num1 * n2, num1 * n1, num1 * num2);
+//   return 0;
+// }
+
+// 10171_고양이
+
+// #include <stdio.h>
+// int main(void)
+// {
+//   printf("\\    /\\\n");
+//   printf(" )  ( ')\n");
+//   printf("(  /  )\n");
+//   printf(" \\(__)|\n");
+//   return 0;
+// }
+
+// 10172_개
+
+// #include <stdio.h>
+// int main(void)
+// {
+//   printf("|\\_/|\n");
+//   printf("|q p|   /}\n");
+//   printf("( 0 )\"\"\"\\\n");
+//   printf("|\"^\"`    |\n");
+//   printf("||_/=\\\\__|\n");
+//   return 0;
+// }
+
+// 25083_새싹
+
+#include <stdio.h>
+int main(void)
+{
+  printf("         ,r\'\"7\n");
+  printf("r`-_   ,'  ,/\n");
+  printf(" \\. \". L_r'\n");
+  printf("   `~\\/\n");
+  printf("      |\n");
+  printf("      |\n");
+  return 0;
+}
